a02745bc91902f61dafbe38e774438768264e5d0
jadhavmahesh012 <jadhavmahesh012@gmail.com>
2024-08-02 10:32:40 +0530

    added

--- DifferentScenarios/010_browserwindowhandling.py
+++ DifferentScenarios/010_browserwindowhandling.py
@@ -7,3 +7,7 @@ from selenium.webdriver.common.by import By
 
 serv_obj=Service("C:\Mahesh\chromedriver-win64\chromedriver.exe")
 driver=webdriver.Chrome(service=serv_obj)
+
+'''switch_to.window(WindowID)
+current_window_handle --Return windowID of single browser window
+window_handle --Return windowID's of multiple browser window'''
